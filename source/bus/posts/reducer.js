287cb7702179c4b795f827953bedd63e430e3a00
d.vakatsiienko <d.vakatsiienko@lectrum.io>
2018-06-02 16:43:21 +0300

    quick update

--- source/bus/posts/reducer.js
+++ source/bus/posts/reducer.js
@@ -1,12 +1,26 @@
+// Core
+import { List, Map } from 'immutable';
+
 // Instruments
 import { types } from './types';
 
-const initialState = [];
+const initialState = List([
+    Map({
+        id:      '123',
+        created: 124857189213,
+        likes:   List([]),
+        author:  Map({
+            firstName: 'Jack',
+            lastName:  'Herer',
+            avatar:
+                'http://i0.kym-cdn.com/entries/icons/original/000/000/774/lime-cat.jpg',
+        }),
+    })
+]);
 
 export const postsReducer = (state = initialState, action) => {
     switch (action.type) {
         case types.FETCH_POSTS_SUCCESS:
-
             return action.payload;
 
         default:
